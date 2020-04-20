0132db9d3fdad65b06333619c5c5efaaacb1ad0d
answerdgh <answerdgh@163.com>
2020-04-20 20:19:11 +0800

    haha

---
+++ mall-demo/src/test/java/com/macro/mall/Test01.java
@@ -0,0 +1,10 @@
+package com.macro.mall;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class Test01 {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
